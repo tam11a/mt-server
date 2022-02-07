eaed6bad1c0f7e24c72022a36b2aa7b01e55c0b7
tam11a-rit <ibrahim.tamim@reserveitbd.com>
2022-02-07 17:09:06 +0600

    first commit

---
+++ pages/api/login.js
@@ -0,0 +1,46 @@
+import { Client } from "@notionhq/client";
+import { cors } from "../../utilities/corsPolicy";
+
+const searchQuery = async (uname, password) => {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.databases.query({
+    database_id: process.env.USER_DB,
+    filter: {
+      and: [
+        {
+          property: "uname",
+          text: {
+            equals: uname,
+          },
+        },
+        {
+          property: "password",
+          text: {
+            equals: password,
+          },
+        },
+      ],
+    },
+  });
+
+  if (response.results.length)
+    return { success: true, userInfo: response.results[0] };
+  else return { success: false };
+};
+
+export default async function handler(req, res) {
+  await cors(req, res);
+  console.log(req.body);
+  if (req.method === "POST") {
+    res.status(200).json(await searchQuery(req.body.uname, req.body.password));
+  } else {
+    res.status(400);
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: true,
+    externalResolver: true,
+  },
+};
---
+++ utilities/corsPolicy.js
@@ -0,0 +1,21 @@
+import Cors from "cors";
+
+export default function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+export const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
